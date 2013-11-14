64501e505bbf22886b2119236f89b73c65af8cde
Kevin <kevin.pheasey@gmail.com>
2013-11-14 16:47:48 -0500

    Guest emails are saved to orders and credit card information saved correctly so credit memos can be created

--- app/code/community/Gemgento/Catalog/Model/Product/Action.php
+++ app/code/community/Gemgento/Catalog/Model/Product/Action.php
@@ -6,26 +6,8 @@
  * @package     Gemgento_Catalog
  * @author      Gemgento Team
  */
-class Gemgento_Catalog_Model_Product_Action extends Mage_Core_Model_Abstract
+class Gemgento_Catalog_Model_Product_Action extends Mage_Catalog_Model_Product_Action
 {
-    /**
-     * Initialize resource model
-     *
-     */
-    protected function _construct()
-    {
-        $this->_init('catalog/product_action');
-    }
-
-    /**
-     * Retrieve resource instance wrapper
-     *
-     * @return Mage_Catalog_Model_Resource_Eav_Mysql4_Product_Action
-     */
-    protected function _getResource()
-    {
-        return parent::_getResource();
-    }
 
     /**
      * Update attribute values for entity list per store
@@ -49,14 +31,6 @@ class Gemgento_Catalog_Model_Product_Action extends Mage_Core_Model_Abstract
             'attributes_data'   => $attrData,
             'store_id'          => $storeId
         ));
-        
-        /// BEGIN Mauigento Update ///
-            
-        /**
-         * By default Magento does not update a product's update_at timestamp
-         * during a mass attribute change.  This timestamp update is needed
-         * for the Gemgento sync to detect the changes.
-         */
 
         $curr_date = date("Y-m-d H:i:s");
 
@@ -68,56 +42,10 @@ class Gemgento_Catalog_Model_Product_Action extends Mage_Core_Model_Abstract
             $product->save();
         }
 
-        /// END Mauigento Update ///
-
         // register mass action indexer event
         Mage::getSingleton('index/indexer')->processEntityAction(
             $this, Mage_Catalog_Model_Product::ENTITY, Mage_Index_Model_Event::TYPE_MASS_ACTION
         );
         return $this;
-    }
-
-    /**
-     * Update websites for product action
-     *
-     * allowed types:
-     * - add
-     * - remove
-     *
-     * @param array $productIds
-     * @param array $websiteIds
-     * @param string $type
-     */
-    public function updateWebsites($productIds, $websiteIds, $type)
-    {
-        Mage::dispatchEvent('catalog_product_website_update_before', array(
-            'website_ids'   => $websiteIds,
-            'product_ids'   => $productIds,
-            'action'        => $type
-        ));
-
-        if ($type == 'add') {
-            Mage::getModel('catalog/product_website')->addProducts($websiteIds, $productIds);
-        } else if ($type == 'remove') {
-            Mage::getModel('catalog/product_website')->removeProducts($websiteIds, $productIds);
-        }
-
-        $this->setData(array(
-            'product_ids' => array_unique($productIds),
-            'website_ids' => $websiteIds,
-            'action_type' => $type
-        ));
-
-        // register mass action indexer event
-        Mage::getSingleton('index/indexer')->processEntityAction(
-            $this, Mage_Catalog_Model_Product::ENTITY, Mage_Index_Model_Event::TYPE_MASS_ACTION
-        );
-
-        // add back compatibility system event
-        Mage::dispatchEvent('catalog_product_website_update', array(
-            'website_ids'   => $websiteIds,
-            'product_ids'   => $productIds,
-            'action'        => $type
-        ));
     }
 }
