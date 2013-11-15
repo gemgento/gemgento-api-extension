6902ac272cac859668092d95a3d65b2e0b738bf3
Kevin <kevin.pheasey@gmail.com>
2013-11-14 19:16:05 -0500

    Save product observer

---
+++ app/code/community/Gemgento/Push/Model/Observer.php
@@ -0,0 +1,13 @@
+<?php
+
+class Gemgento_Push_Model_Observer {
+
+    public function __construct() {
+        
+    }
+
+    public function product_update($observer) {
+        exit(__METHOD__);
+    }
+
+}
