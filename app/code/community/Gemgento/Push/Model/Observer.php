0e9f921f7422f5272bb49aacf4aa876a0576a0fb
Kevin <kevin.pheasey@gmail.com>
2013-11-14 20:43:27 -0500

    Add observers with dummy functions for all needed events

--- app/code/community/Gemgento/Push/Model/Observer.php
+++ app/code/community/Gemgento/Push/Model/Observer.php
@@ -6,8 +6,36 @@ class Gemgento_Push_Model_Observer {
         
     }
 
-    public function product_update($observer) {
-        exit(__METHOD__);
+    public function product($observer) {
+        // TODO: Push product data
+    }
+    
+    public function asset($observer) {
+        // TODO: Push asset data
+    }
+    
+    public function stock($observer) {
+        // TODO: Push stock data
+    }
+    
+    public function category($observer) {
+        // TODO: Push category data
+    }
+    
+    public function attribute_set($observer) {
+        // TODO: Push attribute set data
+    }
+    
+    public function attribute($observer) {
+        // TODO: Push attribute data
+    }
+    
+    public function customer($observer) {
+        // TODO: Push customer data
+    }
+    
+    public function order($observer) {
+        // TODO: Push order data
     }
 
 }
