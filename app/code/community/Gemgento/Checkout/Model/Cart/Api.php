02df0c11c389709012e41dc9f53e98a6fbefa5df
Kevin <kevin.pheasey@gmail.com>
2013-11-19 13:11:50 -0500

    Carts created with API are set to active so they are factored in reporting

---
+++ app/code/community/Gemgento/Checkout/Model/Cart/Api.php
@@ -0,0 +1,27 @@
+<?php
+
+class Gemgento_Checkout_Model_Cart_Api extends Mage_Checkout_Model_Cart_Api {
+
+    /**
+     * Create new quote for shopping cart
+     *
+     * @param int|string $store
+     * @return int
+     */
+    public function create($store = null) {
+        $storeId = $this->_getStoreId($store);
+
+        try {
+            /* @var $quote Mage_Sales_Model_Quote */
+            $quote = Mage::getModel('sales/quote');
+            $quote->setStoreId($storeId)
+                    ->setIsActive(true)
+                    ->setIsMultiShipping(false)
+                    ->save();
+        } catch (Mage_Core_Exception $e) {
+            $this->_fault('create_quote_fault', $e->getMessage());
+        }
+        return (int) $quote->getId();
+    }
+
+}
