d3302d07f1f407ab10afa49b56d5d88b7e8cb7f8
Kevin <kevin.pheasey@gmail.com>
2013-12-10 11:59:08 -0500

    Push gemgento_id with order data

--- app/code/community/Gemgento/Push/Model/Observer.php
+++ app/code/community/Gemgento/Push/Model/Observer.php
@@ -181,6 +181,7 @@ class Gemgento_Push_Model_Observer {
         $data = array();
 
         $data['order_id'] = $order->getId();
+        $data['gemgento_id'] = $order->getGemgentoId();
         $data['store_id'] = $order->getStoreId();
         $data['shipping_address'] = $this->_getAttributes($order->getShippingAddress(), 'order_address');
         $data['billing_address'] = $this->_getAttributes($order->getBillingAddress(), 'order_address');
