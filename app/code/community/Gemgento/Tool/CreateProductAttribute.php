bd18f75efaa2a6c0d6d40953d622d47ce7839cf3
Kevin <kevin.pheasey@gmail.com>
2014-03-04 12:31:36 -0500

    Add gemgento_id product attribute on extension setup

--- app/code/community/Gemgento/Tool/CreateProductAttribute.php
+++ app/code/community/Gemgento/Tool/CreateProductAttribute.php
@@ -8,26 +8,6 @@ $installer = new Mage_Customer_Model_Entity_Setup('core_setup');
 
 $installer->startSetup();
 
-// add gemgento_id to customers
-$vCustomerEntityType = $installer->getEntityTypeId('customer');
-$vCustAttributeSetId = $installer->getDefaultAttributeSetId($vCustomerEntityType);
-$vCustAttributeGroupId = $installer->getDefaultAttributeGroupId($vCustomerEntityType, $vCustAttributeSetId);
-
-$installer->addAttribute('customer', 'gemgento_id', array(
-    'label' => 'Gemgento Id',
-    'input' => 'text',
-    'type' => 'int',
-    'forms' => array('customer_account_edit', 'customer_account_create', 'adminhtml_customer', 'checkout_register'),
-    'required' => 0,
-    'user_defined' => 1,
-));
-
-$installer->addAttributeToGroup($vCustomerEntityType, $vCustAttributeSetId, $vCustAttributeGroupId, 'gemgento_id', 0);
-
-$oAttribute = Mage::getSingleton('eav/config')->getAttribute('customer', 'gemgento_id');
-$oAttribute->setData('used_in_forms', array('customer_account_edit', 'customer_account_create', 'adminhtml_customer', 'checkout_register'));
-$oAttribute->save();
-
 // Add gemgento_id to products
 $attrCode = 'gemgento_id';
 
