6b8d9f2e5aa38a56111c6f651cbc5b41f2a1c0ce
Kevin <kevin.pheasey@gmail.com>
2014-04-21 11:02:54 -0400

    Add payment information to the ServiceQuote instead of the Quote, making cc number available to the final processing of the order

--- app/code/community/Gemgento/Checkout/Model/Cart/Api.php
+++ app/code/community/Gemgento/Checkout/Model/Cart/Api.php
@@ -60,12 +60,6 @@ class Gemgento_Checkout_Model_Cart_Api extends Mage_Checkout_Model_Cart_Api {
             $this->_fault('guest_checkout_is_not_enabled');
         }
         
-        // set payment data again so that credit cards can be processed since they are not stored
-        if ($paymentData != null) {
-            $paymentData = $this->_preparePaymentData($paymentData);
-            $quote->getPayment()->importData($paymentData);    
-        }
-        
         // set the customers ip 
         if ($remoteIp == null) {
             $remoteIp = Mage::helper('core/http')->getRemoteAddr();
@@ -81,6 +75,13 @@ class Gemgento_Checkout_Model_Cart_Api extends Mage_Checkout_Model_Cart_Api {
             $quote->collectTotals();
             /** @var $service Mage_Sales_Model_Service_Quote */
             $service = Mage::getModel('sales/service_quote', $quote);
+            
+            // cc_number and cc_cid are lost because API is stateless, need to add them back
+            if ($paymentData != null) { 
+                $paymentData = $this->_preparePaymentData($paymentData);
+                $service->getQuote()->getPayment()->importData($paymentData);    
+            }
+            
             $service->submitAll();
 
             if ($isNewCustomer) {
