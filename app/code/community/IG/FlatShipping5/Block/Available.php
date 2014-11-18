7e2c603085b984c4799ac4d5bf71adafe7f4165a
Phil Vasilveski <phil@mauinewyork.com>
2014-11-18 01:58:56 +0100

    Extensions were installed

---
+++ app/code/community/IG/FlatShipping5/Block/Available.php
@@ -0,0 +1,36 @@
+<?php 
+/**
+ * IDEALIAGroup srl
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@idealiagroup.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category   IG
+ * @package    IG_FlatShipping5
+ * @copyright  Copyright (c) 2010-2011 IDEALIAGroup srl (http://www.idealiagroup.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Riccardo Tempesta <tempesta@idealiagroup.com>
+ */
+
+class IG_FlatShipping5_Block_Available extends Mage_Checkout_Block_Onepage_Shipping_Method_Available
+{
+	protected function getInfoText($carrierCode)
+	{
+		if ($text = Mage::getStoreConfig('carriers/'.$carrierCode.'/infotext')) {
+            return $text;
+        }
+        return '';
+	}
+}
---
+++ app/code/community/IG/FlatShipping5/Model/Carrier/Flatrate5.php
@@ -0,0 +1,30 @@
+<?php 
+/**
+ * IDEALIAGroup srl
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@idealiagroup.com so we can send you a copy immediately.
+ *
+ * DISCLAIMER
+ *
+ * Do not edit or add to this file if you wish to upgrade this module to newer
+ * versions in the future.
+ *
+ * @category   IG
+ * @package    IG_FlatShipping5
+ * @copyright  Copyright (c) 2010-2011 IDEALIAGroup srl (http://www.idealiagroup.com)
+ * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ * @author     Riccardo Tempesta <tempesta@idealiagroup.com>
+ */
+ 
+class IG_FlatShipping5_Model_Carrier_Flatrate5 extends IG_FlatShipping5_Model_Carrier_Abstract
+{
+	protected $_code = 'flatrate5';
+}
