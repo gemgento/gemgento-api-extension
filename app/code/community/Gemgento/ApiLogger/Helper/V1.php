3921cab82ff37311a91c3ddf4b26ff57bf3b2852
Kevin Pheasey <kevin.pheasey@gmail.com>
2015-07-23 09:50:44 -0400

    Remove ApiLogger module.  It must be installed separately.

--- app/code/community/Gemgento/ApiLogger/Helper/V1.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Gemgento_ApiLogger_Helper_V1 extends Gemgento_ApiLogger_Helper_Data
-{
-    protected $logFileName = 'v1_soap.log';
-}
--- app/code/community/Gemgento/ApiLogger/Helper/V2.php
+++
@@ -1,5 +0,0 @@
-<?php
-class Gemgento_ApiLogger_Helper_V2 extends Gemgento_ApiLogger_Helper_Data
-{
-    protected $logFileName = 'v2_soap.log';
-}
